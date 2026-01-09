4bbdd6ccb63342a76998d944aea576f646b6fa81
deadashx <deadashx@gmail.com>
2026-01-09 20:35:30 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/bestie-birthday/',   // ✅ important for GitHub Pages
+  base: '/',   // ✅ Vercel
 })
